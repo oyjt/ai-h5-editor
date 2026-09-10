9d270c252fd56a6e4a0e9952cc7428b620bf4acb
江阳小道 <oyjt001@gmail.com>
2026-09-10 12:15:09 +0800

    style: load shared design system tokens

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'vant/lib/index.css'
 import 'uno.css'
+import './styles/design-system.css'
 import './styles/global.css'
 
 import App from './App.vue'
