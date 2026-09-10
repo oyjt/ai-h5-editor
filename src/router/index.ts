70ff8efbbb99bf03fda8542e327ade7a041ee14e
江阳小道 <oyjt001@gmail.com>
2026-09-10 11:21:58 +0800

    feat: wire platform routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,9 +9,34 @@ const router = createRouter({
       component: () => import('@/views/EditorView.vue'),
     },
     {
+      path: '/workspace',
+      name: 'workspace',
+      component: () => import('@/views/WorkspaceView.vue'),
+    },
+    {
       path: '/templates',
       name: 'templates',
       component: () => import('@/views/TemplateView.vue'),
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: () => import('@/views/ProjectsView.vue'),
+    },
+    {
+      path: '/assets',
+      name: 'assets',
+      component: () => import('@/views/AssetsView.vue'),
+    },
+    {
+      path: '/analytics',
+      name: 'analytics',
+      component: () => import('@/views/AnalyticsView.vue'),
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/LoginView.vue'),
     },
     {
       path: '/preview',
